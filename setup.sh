4a00284d95d8f8b4b8e2e9c24f881892e577db99
GeraldVargas <202405633@est.umss.edu>
2026-01-12 13:41:26 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,13 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[general]\n\
+email = \"youremail@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
-port = $PORT\n\
-enableCORS = false\n\
 headless = true\n\
-\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
